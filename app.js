125aad19d92537a574bb0c29be446108fb5f4b18
nothembah <74840172+nothembah@users.noreply.github.com>
2021-10-08 16:54:55 +0200

    app

---
+++ app.js
@@ -0,0 +1,11 @@
+var start = document.querySelector('#new');
+var hit = document.querySelector('#hit');
+var end = document.querySelector('#stand');
+function startGame() {
+}
+function produceCard() {
+}
+function hitButton() {
+}
+function standButton() {
+}
--- app.ts
+++ app.ts
@@ -2,3 +2,18 @@ let start = document.querySelector('#new');
 let hit = document.querySelector('#hit');
 let end = document.querySelector('#stand');
 
+function startGame(){ 
+
+}
+
+function produceCard(){
+
+}
+
+function hitButton(){
+
+}
+
+function standButton(){
+
+}
